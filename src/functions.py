47d6f5c50087f52ee74fdbe00c4da54bf0a3bcc4
Ragghav Karunagaran <ragghav01@gmail.com>
2026-04-08 16:21:18 -0400

    Task 2.1: Writing the Equilibrium Kernel Function and Initial Equilibrium State (prior to Streaming & Collisions)

--- src/functions.py
+++ src/functions.py
@@ -14,3 +14,17 @@ u = ti.Vector.field(2, dtype=ti.f32, shape=(nx, ny))
 w = (4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36)
 e_values = ((0,0), (1,0), (0,-1), (-1,0), (0,1), (1,-1), (-1,-1), (-1,1), (1,1))
 e = ti.static([ti.Vector(i) for i in e_values])
+
+# initial fluid state kernel
+@ti.kernel
+def initial_fluid():
+    for i,j in rho:
+
+        rho[i,j] = 1.0
+        u[i, j] = ti.Vector([0.1,0.0])
+
+        for k in ti.static(range(9)):
+            eu = e[k].dot(u[i,j])
+            u2 = u[i,j].dot(u[i,j])
+            fi_equilibirum = w[k]*rho[i,j]*(1+3*eu+4.5*(eu**2)-1.5*u2)
+            f[i, j][k] = fi_equilibirum
